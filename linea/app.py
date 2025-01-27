71c63ddee3e1d4de1e657e75c726eaef7abb9f35
fdcirettg <federico.cirett@unison.mx>
2025-01-27 10:56:41 -0700

    Agregué argparse para usar argumentos m y b

--- linea/app.py
+++ linea/app.py
@@ -1,10 +1,15 @@
 # cálculo de coordenadas de líneas 
 import funciones
+import argparse
 
 
-def main():
-    m = 2.0
-    b = 3.0
+def main(m:float, b:float):
+    '''
+    Función principal que calcula las coordenadas de una línea recta
+    Recibimos m y b
+    Regresa: nada'''
+    #m = 2.0
+    #b = 3.0
     #X = [x for x in range(1,11)]
     #Y = [funciones.calcular_y(x, m, b) for x in X]
     #print("Enteros:")
@@ -19,4 +24,9 @@ def main():
     
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', type=float, help='Pendiente de la línea', default=2.0)
+    parser.add_argument('-b', type=float, help='Ordenada al origen', default=3.0)
+    args = parser.parse_args()
+    main(args.m, args.b)
+    #main(m=2.0, b=3.0)
