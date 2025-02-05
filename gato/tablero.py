f34ae42177dd7f86cd20a9fbd0a47db265672bc3
fdcirettg <federico.cirett@unison.mx>
2025-02-05 14:34:43 -0700

    Condiciones de juego y resultados (Gana y empate)

--- gato/tablero.py
+++ gato/tablero.py
@@ -38,7 +38,7 @@ def usuario(simbolos:dict):
             print('Elija un número del 1 al 9')
 
 def juego(simbolos:dict):
-    ''' Juego del gato '''
+    ''' Juego del gato ''' 
     lista_combinaciones = [
         ['1','2','3'],
         ['4','5','6'],
