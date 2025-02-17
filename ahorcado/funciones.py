9030c00b2b2174d2d05fa1602d9102c0fd173455
fdcirettg <federico.cirett@unison.mx>
2025-02-17 10:23:54 -0700

    adivina_letra retorna nuevo número de oportunidades

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -48,7 +48,7 @@ def obten_palabras(lista:list)->list:
     set_palabras = {unicodedata.normalize('NFKD', palabra).encode('ascii', 'ignore').decode('ascii') for palabra in set_palabras}
     return list(set_palabras)
 
-def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int):
+def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int)->int:
     '''
     Adivina una letra de una palabra
     '''
@@ -59,7 +59,8 @@ def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int):
         else:
             palabra_oculta += "_"
     print(f'Tienes {turnos} oportunidades de fallar')
-    print(f'El abecedario es: {abc}')
+    abcd = ' '.join(abc.values())
+    print(f'El abecedario es: {abcd}')
     print(f'La palabra es: {palabra_oculta}')
     letra = input('Ingresa una letra: ')
     letra = letra.lower()
@@ -72,6 +73,7 @@ def adivina_letra(abc:dict, palabra:str, letras_adivinadas:set, turnos:int):
                 letras_adivinadas.add(letra)
             else:
                 turnos -= 1
+    return turnos
 
 if __name__ == '__main__':
     plantillas = carga_plantillas('plantilla')
@@ -84,5 +86,7 @@ if __name__ == '__main__':
     abcdario = {letra:letra for letra in string.ascii_lowercase}
     adivinadas = set()
     t = 5 # oportunidades
-    adivina_letra(abcdario, p, adivinadas, t)
-    adivina_letra(abcdario, p, adivinadas, t)
+    t = adivina_letra(abcdario, p, adivinadas, t)
+    print(t)
+    t = adivina_letra(abcdario, p, adivinadas, t)
+    print(t)
