1532e41a6452506318d6375ae48a7bc229a08815
fdcirettg <federico.cirett@unison.mx>
2025-02-26 14:48:39 -0700

    Iniciamos toreno con 4 equipos

--- games/main.py
+++ games/main.py
@@ -22,13 +22,33 @@ def main(archivo_torneo:str):
                            'Alba', 'Iniesta', 'Silva', 
                            'Isco', 'Busquets', 'Costa', 
                            'Morata', 'Asensio']
+        players_brasil = ['Neymar', 'Coutinho', 'Marcelo',
+                            'Casemiro', 'Alisson', 'Jesus',
+                            'Paulinho', 'Thiago', 'Silva',
+                            'Firmino', 'Danilo']
+        players_argentina = ['Messi', 'Aguero', 'Di Maria',
+                             'Mascherano', 'Higuain', 'Dybala',
+                             'Otamendi', 'Romero', 'Rojo',
+                             'Banega', 'Fazio']
         lista_mexico = [Athlete(x) for x in players_mexico]
         lista_espania = [Athlete(x) for x in players_espania]
+        lista_brasil = [Athlete(x) for x in players_brasil]
+        lista_argentina = [Athlete(x) for x in players_argentina]  
         soccer = Sport("Soccer", 11, "FIFA")
         mexico = Team("Mexico", soccer, lista_mexico)
         espania = Team("España", soccer, lista_espania)
-        juego = Game(mexico, espania)
-        torneo = [juego.to_json()]
+        brasil = Team("Brasil", soccer, lista_brasil)
+        argentina = Team("Argentina", soccer, lista_argentina)
+        equipos = [mexico, espania, brasil, argentina]
+        d = {}
+        for local in equipos:
+            for visitante in equipos:
+                if local != visitante:
+                    juego = Game(local, visitante)
+                    d[juego.to_json()['A']['name']] = juego.to_json()
+        torneo = list(d.values())
+        #juego = Game(mexico, espania)
+        #torneo = [juego.to_json()]
         archivo_torneo = "torneo.json"
         with open(archivo_torneo, "w", encoding='utf8') as f:
             json.dump(torneo, f, ensure_ascii=False, indent=4)
